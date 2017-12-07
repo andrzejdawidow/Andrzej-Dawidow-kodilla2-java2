918cd89bc3a78c586450baaabf153576bcddc17b
“nasz_login_do_github <nasz_email_do_github>
2017-12-07 16:09:39 +0100

    initial commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        int result1 = calculator.add(6, 4);
+        int result2 = calculator.subtract(6, 4);
+        System.out.println(result1);
+        System.out.println(result2);
+        if ((result1 == 10) && (result2 == 2)) {
+            System.out.println("Calculator is OK");
+        } else {
+            System.out.println("Something is Wrong");
+        }
     }
 }
