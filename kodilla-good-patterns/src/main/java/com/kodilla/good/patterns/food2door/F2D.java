29c7b5ad9ea32f8f56ecf39d874cd1d03cf025aa
AndrzejDawidow <andrzejdawidow@gmail.com>
2018-01-26 22:50:59 +0100

    initial commit

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/F2D.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/F2D.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class F2D implements ProductOrderService {
     List<Supplier> suppliers = new ArrayList<>();
-    public void addSupplier(Supplier cos) {
-        suppliers.add(cos);
+    public void addSupplier(Supplier order) {
+        suppliers.add(order);
     }
 
     public boolean productOrder(String nameSupplier,  String nameProduct, int quantity) {
